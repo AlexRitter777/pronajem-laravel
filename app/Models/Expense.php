31d0fd27fd609046beead446d7948942ea6d503d
Alex <bogachev@email.cz>
2026-08-21 21:29:33 +0200

    feat: expense type relation in the expense type model

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -3,14 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Expense extends Model
 {
-
     protected $fillable = [
         'user_id',
-        'name'
+        'name',
     ];
 
-
+    public function settlementExpenses(): HasMany
+    {
+        return $this->hasMany(SettlementExpense::class, 'expense_type_id');
+    }
 }
